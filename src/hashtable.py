194fff804a64947bf8282f38068a5c039e17b3a6
brettmadrid <brettmadrid9@gmail.com>
2020-01-28 22:13:21 -0800

    added comments to code

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,22 +1,26 @@
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
-    def __init__(self, capacity):
-        self.capacity = capacity  # Number of buckets in the hash table
-        self.storage = [None] * capacity
-        self.count = 0
 
+    def __init__(self, capacity):
+        self.capacity = capacity  # Number of slots in the hash table
+        # pre-allocate an array with a definite size
+        self.storage = [None] * capacity
+        self.count = 0  # number of actual items stored in the hash table
 
     def _hash(self, key):
         '''
@@ -24,8 +28,8 @@ class HashTable:
 
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
+        # using Python's built in hash function returns a hashed value
         return hash(key)
-
 
     def _hash_djb2(self, key):
         '''
@@ -35,14 +39,14 @@ class HashTable:
         '''
         pass
 
-
     def _hash_mod(self, key):
         '''
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
+        # pass the key to our _hash method
+        # modulus by the length of our capacity to return an index that fits
         return self._hash(key) % self.capacity
-
 
     def insert(self, key, value):
         '''
@@ -52,27 +56,38 @@ class HashTable:
 
         Fill this in.
         '''
+
+        if self.capacity == self.count - 1:
+            self.resize()
+
+        # first hash the key to get an index value
         index = self._hash_mod(key)
 
+        # now create a new linkedPair node to hold the key, value data
+        new_node = LinkedPair(key, value)
+
+        # if that index location is available, add the key and value as a LinkedPair object
         if self.storage[index] == None:
-            self.storage[index] = LinkedPair(key, value)
+            # instantiates a LinkedPair object and stores in the storage array
+            self.storage[index] = new_node
             self.count += 1
-        else:
+        else:  # if that index location is already taken
+            # store what is already there into a temp variable
             temp = self.storage[index]
-            if temp.key == key:
+            if temp.key == key:  # if the new key is the same as the existing key
+                # then overwrite with the new value
                 self.storage[index].value = value
                 return None
-            else:
-                while temp.next != None:
+            else:  # if the new key is not the same as the key already there
+                while temp.next != None:  # loop to the end of the linked list
                     temp = temp.next
-                    if temp.key == key:
-                        temp.value = value
+                    if temp.key == key:  # checking each item in the linked list to see if the key is already stored
+                        temp.value = value  # if the key is already stored, update with the new value
                         return None
-            temp.next = LinkedPair(key, value)
-            self.count += 1
+            temp.next = new_node  # once at the end of the linked list, add the new LinkedPair
+            self.count += 1  # increment count of items in the hash table
 
         return None
-
 
     def remove(self, key):
         '''
@@ -83,19 +98,19 @@ class HashTable:
         Fill this in.
         '''
         index = self._hash_mod(key)
-        current = self.storage[index]
+        current = self.storage[index]  # retrieve what is at that index
 
         if current is None:
             print(f'Warning! {key} is not in the hash table')
             return None
         else:
+            # if a value is there, store it in a temp variable
             temp = self.storage[index]
 
             if temp.next != None:
-                if temp.next.key == key:
-                    temp.next = temp.next.next
+                if temp.next.key == key:  # check for a match
+                    temp.next = temp.next.next  # reassign
                     self.count -= 1
-                    break
                 temp = temp.next
             else:
                 if temp.key == key:
@@ -103,7 +118,6 @@ class HashTable:
                     return None
 
             return None
-
 
     def retrieve(self, key):
         '''
@@ -114,16 +128,16 @@ class HashTable:
         Fill this in.
         '''
         index = self._hash_mod(key)
-        if self.storage[index] != None: # if the value is found
-            temp = self.storage[index]  # get the proper index
+        if self.storage[index] != None:  # if the value is found
+            # get the linked list or value stored at that index into a variable
+            temp = self.storage[index]
 
-            while temp != None:
-                if temp.key == key:
-                    return temp.value
-                temp = temp.next # go to next item in linked list
-        
-        return None
+            while temp != None:  # loop through the linked list
+                if temp.key == key:  # compare each key
+                    return temp.value  # if found, return the value
+                temp = temp.next  # go to next item in linked list
 
+        return None  # else not found
 
     def resize(self):
         '''
@@ -132,9 +146,11 @@ class HashTable:
 
         Fill this in.
         '''
-        new_hash_table = HashTable(self.capacity*2)
-        return new_hash_table
+        self.capacity = self.capacity * 2
+        new_storage = [None] * self.capacity
 
+        for i in range(self.count):
+            new_storage[i] = self.storage[i]
 
 
 if __name__ == "__main__":
